12aeff1425cd7f8db72d54775956d82d12d7867c
GajanandTiwari <gajanandt43@gmail.com>
2025-04-21 10:10:22 +0530

    Add files via upload

---
+++ ExceptionHandling/src/firstONe.java
@@ -0,0 +1,12 @@
+
+public class firstONe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String aa="30";
+int bb=Integer.parseInt(aa);
+bb++;
+System.out.println(bb);
+	}
+
+}
